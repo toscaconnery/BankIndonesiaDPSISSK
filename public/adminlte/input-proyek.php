af9f615672d68a057acd7752f7327a0cdef70d13
toscayconnery <tosca14@mhs.if.its.ac.id>
2017-06-15 11:07:11 +0700

    form input protek mock up

--- public/adminlte/input-proyek.php
+++ public/adminlte/input-proyek.php
@@ -24,12 +24,92 @@
   <?php include 'navbar.php';?>
   <div class="content-wrapper">
     <section class="content-header">
-      <h1>
-        Dashboard
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-home"></i> Dashboard</a></li>
-      </ol>
+      <div class="col-md-12">
+        <h1>
+          Input Proyek
+        </h1>
+      </div>
+      <br>
+      <div class="col-md-8">
+        <!-- Horizontal Form -->
+        <div class="box box-info">
+          <div class="box-header with-border">
+            <h3 class="box-title">Horizontal Form</h3>
+          </div>
+          <!-- /.box-header -->
+          <!-- form start -->
+          <form class="form-horizontal">
+            <div class="box-body">
+              
+              <!--Name-->
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Name</label>
+
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+              </div>
+
+              <!--Category-->
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Category</label>
+
+                <div class="col-sm-10">
+                  <!-- <input type="text" class="form-control" id="inputEmail3"> -->
+                  <select class="form-control select2" style="width: 100%;">
+                  <option>Pilihan 1</option>
+                  <option>Pilihan 2</option>
+                  <option>Pilihan 3</option>
+                </select>
+                </div>
+              </div>
+
+              <!--PIC-->
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">PIC</label>
+
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputEmail3">
+                </div>
+              </div>
+
+              <!--Date-->
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Date</label>
+
+                <div class="col-sm-5">
+                  <input type="date" class="form-control" id="inputEmail3">
+                </div>
+
+                <div class="col-sm-5">
+                  <input type="date" class="form-control" id="inputEmail3">
+                </div>
+                
+              </div>
+
+
+
+
+              <!-- <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox"> Remember me
+                    </label>
+                  </div>
+                </div>
+              </div> -->
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+              <button type="submit" class="btn btn-default">Cancel</button>
+              <button type="submit" class="btn btn-info pull-right">Submit</button>
+            </div>
+            <!-- /.box-footer -->
+          </form>
+        </div>
+        <!-- /.box -->
+      </div>
     </section>
     <br>
   </div>
@@ -49,6 +129,7 @@
 <script src="plugins/knob/jquery.knob.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
 <script src="plugins/daterangepicker/daterangepicker.js"></script>
+<script src="plugins/daterangepicker/daterangepicker.js"></script>
 <script src="plugins/datepicker/bootstrap-datepicker.js"></script>
 <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
@@ -56,5 +137,12 @@
 <script src="dist/js/app.min.js"></script>
 <script src="dist/js/pages/dashboard.js"></script>
 <script src="dist/js/demo.js"></script>
+<script src="plugins/select2/select2.full.min.js"></script>
+<script src="plugins/input-mask/jquery.inputmask.js"></script>
+<script src="plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
+<script src="plugins/input-mask/jquery.inputmask.extensions.js"></script>
+<script src="plugins/colorpicker/bootstrap-colorpicker.min.js"></script>
+<script src="../../plugins/timepicker/bootstrap-timepicker.min.js"></script>
+<script src="../../plugins/iCheck/icheck.min.js"></script>
 </body>
 </html>
