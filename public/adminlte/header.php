d62f94cd438b9f54cf75c41c32a1daadc6eefd6a
faishal15 <faishal15@gmail.com>
2017-06-15 16:49:14 +0700

    perbaikan header

--- public/adminlte/header.php
+++ public/adminlte/header.php
@@ -1,9 +1,9 @@
 <header class="main-header">
   <a href="index.html" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini"><b>R</b>R</span>
+    <span class="logo-mini"><b>SI</b>PRMS</span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg"><b>Reservation</b>Room</span>
+    <span class="logo-lg"><b>SI</b>PMO&RMS</span>
   </a>
   <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top">
