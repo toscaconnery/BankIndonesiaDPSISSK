47e1e6db50ee706afe8a48540b04382e03684850
toscayconnery <tosca14@mhs.if.its.ac.id>
2017-06-16 14:36:58 +0700

    model proyek

---
+++ app/proyek.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class proyek extends Model
+{
+    protected $fillable = [
+    	'nama', 'pic', 'keterangan', 'tanggal_mulai', 'tanggal_selesai', 'real_tanggal_mulai', 'real_tanggal_selesai', 'status',
+    ];
+    
+}
