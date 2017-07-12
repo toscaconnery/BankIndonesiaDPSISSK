d4d196a7259a9d959ff2b155487c6c8c01ca4169
toscayconnery <tosca14@mhs.if.its.ac.id>
2017-07-12 08:42:38 +0700

    database tahun

---
+++ app/Tahun.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tahun extends Model
+{
+	protected $table = 'tahun';
+	
+    protected $fillable = [
+    	'tahun',
+    ];
+}
